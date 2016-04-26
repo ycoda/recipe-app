e79ab9bd6ebe5916c93bff3f37264722db34dafc
ycoda <tankaku2003@yahoo.co.jp>
2016-04-26 20:14:46 +0900

    foo

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,15 +1,18 @@
 class TopController < ApplicationController
-before_action :correct_user, only: :index
+#before_action :correct_user, only: :index
   def index
     @user_panel = UserPanel.all
     #find(params[:id])
   end
 
-
+=begin
   private
     def correct_user
       @user_panel = current_user.user_panels.find_by(id: params[:user_id])
       #@user_panel = .find_by(id: params[:id])
 
     end
+
+=end
+
 end
