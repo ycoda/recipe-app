8cccb38e4d896f3c2855d85707e8850a465a9ffe
ycoda <tankaku2003@yahoo.co.jp>
2016-05-17 13:33:39 +0900

    test-1

--- app/models/basic_recipe.rb
+++
@@ -1,3 +0,0 @@
-class BasicRecipe < ActiveRecord::Base
-
-end
--- app/models/relationship.rb
+++
@@ -1,7 +0,0 @@
-class Relationship < ActiveRecord::Base
-  belongs_to :user, class_name: "User"
-  belongs_to :user_panel, class_name: "UserPanel"
-  validates :user_id, presence: true
-  validates :user_panel_id, presence: true
-
-end
