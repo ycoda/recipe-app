23c194e3a2651d15db6360ae43e4995722c20858
ycoda <tankaku2003@yahoo.co.jp>
2016-06-11 18:21:49 +0900

    fix16 migration remove not null seiyaku

--- db/migrate/20160609072720_change_column_not_null_to_user_panels.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnNotNullToUserPanels < ActiveRecord::Migration
-  def change
-    #. not nullを許可する
-      change_column :user_panels, :title, :string, null: true
-      change_column :user_panels, :ingredient, :string, null: true
-      change_column :user_panels, :seasoning, :string, null: true
-  end
-end
