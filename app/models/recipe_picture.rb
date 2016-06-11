7ef692e6beb9d664373ae2fa47f91c8b30b69638
ycoda <tankaku2003@yahoo.co.jp>
2016-06-11 17:30:02 +0900

    fix14 DB into info all clear

--- app/models/recipe_picture.rb
+++
@@ -1,4 +0,0 @@
-class RecipePicture < ActiveRecord::Base
-  #has_many :user_panels
-  #has_many :users #, through: :user_panels
-end
--- db/migrate/20160512085841_create_recipe_pictures.rb
+++
@@ -1,9 +0,0 @@
-class CreateRecipePictures < ActiveRecord::Migration
-  def change
-    create_table :recipe_pictures do |t|
-      t.string :picture_name
-
-      t.timestamps
-    end
-  end
-end
